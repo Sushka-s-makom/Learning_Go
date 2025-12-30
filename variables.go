24b9eef53a8b3ac80a5ae0d7b367da482fc5f28e
Sushka-s-makom <zabrodskiyalex111206@gmail.com>
2025-12-30 13:46:34 +0300

    Print variables

---
+++ variables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a string = "intial"
+	fmt.Println(a)
+
+	var b, c int = 11, 18
+	fmt.Println(b, c)
+
+	var p int
+	fmt.Println(p)
+
+	//сокращения для объявления инициализированной переменной
+
+	f := "short"
+	fmt.Println(f)
+
+}
