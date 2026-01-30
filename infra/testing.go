6f3fc3db46d9318ce25ac213cb4f367f67eaa774
Sushka-s-makom <zabrodskiyalex111206@gmail.com>
2026-01-31 01:36:27 +0300

    chore : add testing basic func

---
+++ infra/testing.go
@@ -0,0 +1,43 @@
+package infra
+
+import (
+	"fmt"
+	"testing"
+)
+
+func IntMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func TestIntMinBasic(t *testing.T) {
+	ans := IntMin(2, -2)
+	if ans != -2 {
+		t.Errorf("Expected %d, got %d", -2, ans)
+	}
+}
+
+// часто в go прогоняют код через табличные тесты
+func TestIntMinTableDriven(t *testing.T) {
+	var tests = []struct {
+		a, b int
+		want int
+	}{
+		{1, 2, 1},
+		{2, 1, 1},
+		{2, 2, 1},
+		{2, 3, 1},
+	}
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%d,%d", tt.a, tt.b)
+		t.Run(testname, func(t *testing.T) {
+			ans := IntMin(tt.a, tt.b)
+			if ans != tt.want {
+				t.Errorf("Expected %d, got %d", tt.want, ans)
+			}
+		})
+
+	}
+}
